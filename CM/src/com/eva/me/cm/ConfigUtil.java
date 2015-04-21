aa7be3e9b427c4c692f36d516843b1a20de8db40
Minghao Xu <xianyuhuanyue@gmail.com>
2015-04-21 17:13:53 +0800

    add method setProperty()

--- CM/src/com/eva/me/cm/ConfigUtil.java
+++ CM/src/com/eva/me/cm/ConfigUtil.java
@@ -2,8 +2,10 @@ package com.eva.me.cm;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
@@ -77,5 +79,27 @@ public class ConfigUtil{
 		}
 		value = configProperties.getProperty(key);
 		return value;
+	}
+	
+	/**
+	 * Set Properties with key value
+	 * @param key
+	 * @param value
+	 */
+	public void setProperty(String key, String value) {
+		if (configProperties == null) {
+			configProperties = new Properties();
+		}
+		try {
+			OutputStream outputStream = new FileOutputStream(configFilePath);
+			configProperties.setProperty(key, value);
+			configProperties.store(outputStream, null);
+		} catch (FileNotFoundException e) {
+			System.out.println("FileNotFoundException");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IOException: store failed...");
+			e.printStackTrace();
+		}
 	}
 }
