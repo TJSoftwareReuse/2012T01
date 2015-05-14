fa5e41281df2de21b43c368747fd97aeb3487542
Minghao Xu <xianyuhuanyue@gmail.com>
2015-05-14 16:39:13 +0800

    finish structure... add sychonized....

--- Server/src/com/eva/me/server/ClientProcessThread.java
+++ Server/src/com/eva/me/server/ClientProcessThread.java
@@ -1,10 +1,17 @@
 package com.eva.me.server;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
 
 public class ClientProcessThread extends Thread{
 	
 	private Socket clientSocket = null;
+	private InputStream inputStream = null;
+	private OutputStream outputStream = null;
+	
 	
 	public static void log(String logContent) {
 		System.out.println(logContent);
@@ -12,15 +19,34 @@ public class ClientProcessThread extends Thread{
 	
 	public ClientProcessThread(Socket clientSocket) {
 		this.clientSocket = clientSocket;
+		try {
+			inputStream = clientSocket.getInputStream();
+			outputStream = clientSocket.getOutputStream();
+		} catch (IOException e) {
+			log("inputStream | outputStream init error....");
+			e.printStackTrace();
+		}
+		
+		InetAddress inetAddress = clientSocket.getInetAddress();
+		int port = clientSocket.getPort();
+		log("Client connect in...\n" +
+				"Client info:\n" +
+				"\tIP:PORT = "+inetAddress+" : "+port);
 	}
 	
 	@Override
 	public void run() {
 		super.run();
 		
-		log("\nNew Client Request Process Thread create.....\n\n");
+		synchronized (Server.serverLock) {
+			log("\nNew Client Request Process Thread create.....\n\n");
+			processClientInfo();
+			log("\nNew Client Request Process Complete.....\n\n");
+		}
 		
-		
+	}
+
+	private void processClientInfo() {
 		
 	}
 	
--- Server/src/com/eva/me/server/Server.java
+++ Server/src/com/eva/me/server/Server.java
@@ -2,28 +2,34 @@ package com.eva.me.server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server extends ServerSocket{
 	private final static int SERVER_PORT = 12234;
 	private static Server mServer = null;
+	public static Object serverLock = new Object(); 
+	
+	public static void log(String str) {
+		System.out.println(str);
+	}
 	
 	public Server() throws IOException {
 		super(SERVER_PORT);
 		
 		while (true) {
-			
+			Socket clientSocket = accept();
+			ClientProcessThread clientThread = new ClientProcessThread(clientSocket);
 		}
 		
 	}
 	
 	
-	
-	
 	public static void startServer() {
 		try {
 			mServer = new Server();
+			log("Start Server....");
 		} catch (IOException e) {
-			System.out.println("Server Start Exception...");
+			log("Server Start Exception...");
 			e.printStackTrace();
 		}
 	}
@@ -32,9 +38,10 @@ public class Server extends ServerSocket{
 		try {
 			if (mServer != null) {
 				mServer.close();
+				log("Server Close......");
 			}
 		} catch (IOException e) {
-			System.out.println("Server Stop Exception...");
+			log("Server Stop Exception...");
 			e.printStackTrace();
 		}
 	}
