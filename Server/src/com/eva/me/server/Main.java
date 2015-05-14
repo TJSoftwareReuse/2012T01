a4a8e343691385337dc66352cb81b7a8fa08d684
Minghao Xu <xianyuhuanyue@gmail.com>
2015-05-14 15:53:15 +0800

    add start stop server method

---
+++ Server/src/com/eva/me/server/Main.java
@@ -0,0 +1,7 @@
+package com.eva.me.server;
+
+public class Main {
+	public static void main(String [] args) {
+		
+	}
+}
---
+++ Server/src/com/eva/me/server/Server.java
@@ -0,0 +1,37 @@
+package com.eva.me.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Server extends ServerSocket{
+	private final static int SERVER_PORT = 12234;
+	private static Server mServer = null;
+	
+	public Server() throws IOException {
+		super(SERVER_PORT);
+		
+	}
+	
+	
+	
+	
+	public static void startServer() {
+		try {
+			mServer = new Server();
+		} catch (IOException e) {
+			System.out.println("Server Start Exception...");
+			e.printStackTrace();
+		}
+	}
+	
+	public static void stopServer() {
+		try {
+			if (mServer != null) {
+				mServer.close();
+			}
+		} catch (IOException e) {
+			System.out.println("Server Stop Exception...");
+			e.printStackTrace();
+		}
+	}
+}
