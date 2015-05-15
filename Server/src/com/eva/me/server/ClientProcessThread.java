19fee267ce08a2046b5888db8613ed358317de29
Minghao Xu <xianyuhuanyue@gmail.com>
2015-05-15 10:49:43 +0800

    finish method transInputStreamToStr..with encoding

--- Server/src/com/eva/me/server/ClientProcessThread.java
+++ Server/src/com/eva/me/server/ClientProcessThread.java
@@ -1,16 +1,24 @@
 package com.eva.me.server;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
 import java.net.Socket;
 
 public class ClientProcessThread extends Thread{
 	
+	private final String encoding = "utf-8";
+	
 	private Socket clientSocket = null;
 	private InputStream inputStream = null;
 	private OutputStream outputStream = null;
+	
+	private final String response = null;
+	private final String request; 
 	
 	
 	public static void log(String logContent) {
@@ -32,8 +40,36 @@ public class ClientProcessThread extends Thread{
 		log("Client connect in...\n" +
 				"Client info:\n" +
 				"\tIP:PORT = "+inetAddress+" : "+port);
+		
+		request = transInputStreamToStr(inputStream);
+		
 	}
 	
+	private String transInputStreamToStr(InputStream iStream) {
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		try {
+			final int BUFFER_SIZE = 1024;
+			byte[] data = new byte[BUFFER_SIZE];
+			int count =-1;
+			while((count = iStream.read(data)) != -1) {
+					byteArrayOutputStream.write(data);
+			}
+			byteArrayOutputStream.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		byte[] dataAll = byteArrayOutputStream.toByteArray();
+		String result = "";
+		try {
+			result = new String(dataAll,0,dataAll.length,encoding);
+		} catch (UnsupportedEncodingException e) {
+			System.out.println("UnsupportedEncodingException.....");
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+
 	@Override
 	public void run() {
 		super.run();
