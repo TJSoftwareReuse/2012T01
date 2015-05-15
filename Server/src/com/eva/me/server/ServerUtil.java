2acb10c5a4b236b48f929e5898c5847d17a91798
Minghao Xu <xianyuhuanyue@gmail.com>
2015-05-15 15:22:28 +0800

    PM Method can not finish!

--- Server/src/com/eva/me/server/ServerUtil.java
+++ Server/src/com/eva/me/server/ServerUtil.java
@@ -1,5 +1,30 @@
 package com.eva.me.server;
 
-public class ServerUtil {
+import src.com.team8.License.License;
 
+public class ServerUtil {
+	private static final int LICENSE_MAX_SIZE = 10;
+	private static final License license = new License(LICENSE_MAX_SIZE);
+	
+	private static void log(String str) {
+		System.out.println(str);
+	}
+	
+	public static void doCMPart() {
+		
+	}
+	
+	public static boolean doLiensePart() {
+		log("License remain: "+license.getRemain()+" / "+license.getLicense());
+		return license.inLicense();
+	}
+	
+	public static void doFMPart() {
+		
+	}
+	
+	public static void doPMPart() {
+		
+	}
+	
 }
