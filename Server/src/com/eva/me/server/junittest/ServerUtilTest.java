1bcf613c00539553d815c62d583f4a63f8a2862e
Minghao Xu <xianyuhuanyue@gmail.com>
2015-05-15 17:44:03 +0800

    Add junit4 test suite

---
+++ Server/src/com/eva/me/server/junittest/ServerUtilTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.eva.me.server.junittest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Zing
+ *
+ */
+public class ServerUtilTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
