a221a06cfe3ac298bb945faa4e411e0aae95c221
Minghao Xu <xianyuhuanyue@gmail.com>
2015-05-27 11:48:51 +0800

    change my client python program, update group name

--- client.py
+++ client.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 import socket, sys, chardet
 
-if len(sys.argv) != 4:
+if len(sys.argv) != 3:
     print 'Parameter not mate, notice must use like this:'
-    print '\rpython client.py [ip] [port] [member_name]'
+    print '\rpython client.py [ip] [port]'
+    # print '\rpython client.py [ip] [port] [member_name]'
     sys.exit(0)
 
 ip = sys.argv[1]
 port = sys.argv[2]
-member = sys.argv[3]
+# member = sys.argv[3]
 
-print chardet.detect(member)
+# print chardet.detect(member)
 
 s = socket.socket()
 # addr = ('192.168.56.1', 12234)
@@ -18,7 +19,8 @@ addr = (ip, int(port))
 
 s.connect(addr)
 # member ='许铭淏'
-s.send(member.encode('IBM855'))
+member = '第一组'
+s.send(member)
 
 data = s.recv(1024)
 print 'data: ',data.decode('utf-8')
